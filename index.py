5eb1b8ae77f9106a6bc5e8d3cf0e9b35f015c20b
ehiaig <ehiagheaigg@gmail.com>
2017-09-18 15:16:54 +0000

    Accepting contact input

---
+++ index.py
@@ -0,0 +1,18 @@
+class Contact:
+    def __init__(self, name, phone, gender, email, postal_address):
+        self.name = name
+        self.phone = phone
+        self.gender = gender
+        self.email = email
+        self.postal_address = postal_address
+
+    def new_contact(self):
+        print('Name: "{}"  Phone Number: "{}" Gender: "{}" Email: "{}" Address:"{}"'.format(self.name, self.phone, self.gender, self.email, self.postal_address))
+
+name = input("Enter name: ")
+phone = input("Enter Phone: ")
+gender = input("Enter Gender: ")
+email = input("Enter Email: ")
+postal_address = input("Enter Postal Address: ")
+my_contact = Contact(name, phone, gender, email, postal_address)
+my_contact.new_contact()
